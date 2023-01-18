f24e75200c0f6f6f33fd0da159040617c8831db7
Samuel <samuelsoutosilva14@gmail.com>
2023-01-17 21:14:39 -0300

    feat: add todo repository

--- src/modules/todo/domain/repository/todo.repository.ts
+++ src/modules/todo/domain/repository/todo.repository.ts
@@ -1,5 +1,26 @@
-import { RepositoryInterface } from '@shared/domain/repository/repository-contracts'
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface
+} from '../../../../@shared/domain/repository/repository-contracts'
 import { Todo } from '../entities/todo'
 
-export default interface TodoRepository
-  extends RepositoryInterface<Todo> { }
+export namespace TodoRepository {
+  export type Filter = string
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+  export class SearchResult extends DefaultSearchResult<Todo, Filter> {}
+
+  export interface Repository extends SearchableRepositoryInterface<
+  Todo,
+  Filter,
+  SearchParams,
+  SearchResult
+  > {
+    exists: (name: string) => Promise<boolean>
+  }
+}
+
+// export default TodoRepository
