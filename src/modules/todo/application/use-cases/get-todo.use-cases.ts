fa1705564355c82be4ad735efbbb35df5e08010c
Samuel <samuelsoutosilva14@gmail.com>
2023-01-17 21:17:01 -0300

    refactor: adds todo mapper as output of execute

--- src/modules/todo/application/use-cases/get-todo.use-cases.ts
+++ src/modules/todo/application/use-cases/get-todo.use-cases.ts
@@ -1,21 +1,15 @@
-import TodoRepository from '../../../../modules/todo/domain/repository/todo.repository'
-import { TodoOutput } from '../dto/todo-output.dto'
+import { TodoRepository } from '../../../../modules/todo/domain/repository/todo.repository'
+import { TodoOutput } from '../dtos/todo-output.dto'
 import UseCase from '../../../../@shared/application/use-cases/use-case'
+import { TodoOutputMapper } from '../mappers/todo-output.mapper'
 
 export default class GetTodoUseCase implements UseCase<Input, Output> {
-  constructor (private readonly todoRepo: TodoRepository) { }
+  constructor (private readonly todoRepo: TodoRepository.Repository) { }
 
   async execute (input: Input): Promise<Output> {
     const entity = await this.todoRepo.findById(input.id)
 
-    return {
-      id: entity.id,
-      title: entity.title,
-      description: entity.description,
-      priority: entity.priority,
-      is_scratched: entity.is_scratched,
-      created_at: entity.created_at as Date
-    }
+    return TodoOutputMapper.toOutput(entity)
   }
 }
 
