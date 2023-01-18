2fff8f7d9419c6fd008df54f2344baecf18addf0
Samuel <samuelsoutosilva14@gmail.com>
2023-01-18 19:18:55 -0300

    feat: add delete todo - use case

---
+++ src/modules/todo/application/use-cases/delete-todo.use-cases.ts
@@ -0,0 +1,17 @@
+import { TodoRepository } from '../../domain/repository/todo.repository'
+import UseCase from '../../../../@shared/application/use-cases/use-case'
+
+export default class DeleteTodoUseCase implements UseCase<Input, void> {
+  constructor (private readonly todoRepo: TodoRepository.Repository) {}
+
+  async execute (input: Input): Promise<void> {
+    const entity = await this.todoRepo.findById(input.id)
+    await this.todoRepo.delete(entity.id)
+  }
+}
+
+export interface Input {
+  id: string
+}
+
+// type Output = void
