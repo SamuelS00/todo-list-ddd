3a02749492350ca7df8fc8b481f5f437cc9312c9
Samuel <samuelsoutosilva14@gmail.com>
2023-01-22 20:16:02 -0300

    feat: add todos controller with crud methods

--- src/nestjs/src/todos/todos.controller.ts
+++ src/nestjs/src/todos/todos.controller.ts
@@ -1,34 +1,66 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TodosService } from './todos.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  Inject,
+  HttpCode,
+  Query,
+} from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import {
+  CreateTodoUseCase,
+  DeleteTodoUseCase,
+  GetTodoUseCase,
+  ListTodosUseCase,
+  UpdateTodoUseCase,
+} from 'todo-list/todo/application';
+import { SearchTodoDto } from './dto/search-todo.dto';
 
 @Controller('todos')
 export class TodosController {
-  constructor(private readonly todosService: TodosService) {}
+  @Inject(CreateTodoUseCase.UseCase)
+  private createUseCase: CreateTodoUseCase.UseCase;
+
+  @Inject(GetTodoUseCase.UseCase)
+  private getUseCase: GetTodoUseCase.UseCase;
+
+  @Inject(ListTodosUseCase.UseCase)
+  private listUseCase: ListTodosUseCase.UseCase;
+
+  @Inject(UpdateTodoUseCase.UseCase)
+  private updateUseCase: UpdateTodoUseCase.UseCase;
+
+  @Inject(DeleteTodoUseCase.UseCase)
+  private deleteUseCase: DeleteTodoUseCase.UseCase;
 
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todosService.create(createTodoDto);
+    return this.createUseCase.execute(createTodoDto);
   }
 
   @Get()
-  findAll() {
-    return this.todosService.findAll();
+  search(@Query() searchParams: SearchTodoDto) {
+    return this.listUseCase.execute(searchParams);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.todosService.findOne(+id);
+    return this.getUseCase.execute({ id });
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todosService.update(+id, updateTodoDto);
+    return this.updateUseCase.execute({ ...updateTodoDto, id });
   }
 
+  @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.todosService.remove(+id);
+    return this.deleteUseCase.execute({ id });
   }
 }
