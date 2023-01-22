a2ef105c5102f957942b71b8ba6739ece4467232
Samuel <samuelsoutosilva14@gmail.com>
2023-01-22 20:15:05 -0300

    feat: create todo dto

--- src/nestjs/src/todos/dto/create-todo.dto.ts
+++ src/nestjs/src/todos/dto/create-todo.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTodoDto {}
+import { CreateTodoUseCase } from 'todo-list/todo/application';
+import { Priority } from 'todo-list/todo/domain';
+
+export class CreateTodoDto implements CreateTodoUseCase.Input {
+  title: string;
+  priority: Priority;
+  description?: string;
+  is_scratched?: boolean;
+}
