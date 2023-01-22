7e9d2af24472a9d216177a21d35668c12e820e56
Samuel <samuelsoutosilva14@gmail.com>
2023-01-22 20:16:16 -0300

    feat: add todo modules

--- src/nestjs/src/todos/todos.module.ts
+++ src/nestjs/src/todos/todos.module.ts
@@ -2,8 +2,59 @@ import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
 
+import { TodoInMemoryRepository } from 'todo-list/todo/infrastructure';
+import { TodoRepository } from 'todo-list/todo/domain';
+import {
+  CreateTodoUseCase,
+  DeleteTodoUseCase,
+  GetTodoUseCase,
+  ListTodosUseCase,
+  UpdateTodoUseCase,
+} from 'todo-list/todo/application';
+
 @Module({
   controllers: [TodosController],
-  providers: [TodosService]
+  providers: [
+    TodosService,
+    {
+      provide: 'TodoInMemoryRepository',
+      useClass: TodoInMemoryRepository,
+    },
+    {
+      provide: CreateTodoUseCase.UseCase,
+      useFactory: (todoRepo: TodoRepository.Repository) => {
+        return new CreateTodoUseCase.UseCase(todoRepo);
+      },
+      inject: ['TodoInMemoryRepository'],
+    },
+    {
+      provide: DeleteTodoUseCase.UseCase,
+      useFactory: (todoRepo: TodoRepository.Repository) => {
+        return new DeleteTodoUseCase.UseCase(todoRepo);
+      },
+      inject: ['TodoInMemoryRepository'],
+    },
+    {
+      provide: GetTodoUseCase.UseCase,
+      useFactory: (todoRepo: TodoRepository.Repository) => {
+        return new GetTodoUseCase.UseCase(todoRepo);
+      },
+      inject: ['TodoInMemoryRepository'],
+    },
+    {
+      provide: ListTodosUseCase.UseCase,
+      useFactory: (todoRepo: TodoRepository.Repository) => {
+        return new ListTodosUseCase.UseCase(todoRepo);
+      },
+      inject: ['TodoInMemoryRepository'],
+    },
+    {
+      provide: UpdateTodoUseCase.UseCase,
+      useFactory: (todoRepo: TodoRepository.Repository) => {
+        return new UpdateTodoUseCase.UseCase(todoRepo);
+      },
+      inject: ['TodoInMemoryRepository'],
+    },
+  ],
 })
 export class TodosModule {}
