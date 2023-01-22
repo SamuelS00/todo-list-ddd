3f9e29a4a5678ec5b56abdb816622a57770375ab
Samuel <samuelsoutosilva14@gmail.com>
2023-01-22 20:14:57 -0300

    feat: add update todo dto

--- src/nestjs/src/todos/dto/update-todo.dto.ts
+++ src/nestjs/src/todos/dto/update-todo.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTodoDto } from './create-todo.dto';
+import { UpdateTodoUseCase } from 'todo-list/todo/application';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto implements Omit<UpdateTodoUseCase.Input, 'id'> {
+  title: string;
+  priority: 'low' | 'medium' | 'high';
+  description?: string;
+  is_scratched?: boolean;
+}
