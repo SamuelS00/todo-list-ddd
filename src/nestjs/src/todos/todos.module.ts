d3e06c1e34545b7c482eb927fab836b40c00c151
Samuel <samuelsoutosilva14@gmail.com>
2023-01-23 11:25:26 -0300

    refactor: add todo providers in modules

--- src/nestjs/src/todos/todos.module.ts
+++ src/nestjs/src/todos/todos.module.ts
@@ -1,60 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
-
-import { TodoInMemoryRepository } from 'todo-list/todo/infrastructure';
-import { TodoRepository } from 'todo-list/todo/domain';
-import {
-  CreateTodoUseCase,
-  DeleteTodoUseCase,
-  GetTodoUseCase,
-  ListTodosUseCase,
-  UpdateTodoUseCase,
-} from 'todo-list/todo/application';
+import { TODO_PROVIDERS } from './providers/todo.providers';
 
 @Module({
   controllers: [TodosController],
   providers: [
     TodosService,
-    {
-      provide: 'TodoInMemoryRepository',
-      useClass: TodoInMemoryRepository,
-    },
-    {
-      provide: CreateTodoUseCase.UseCase,
-      useFactory: (todoRepo: TodoRepository.Repository) => {
-        return new CreateTodoUseCase.UseCase(todoRepo);
-      },
-      inject: ['TodoInMemoryRepository'],
-    },
-    {
-      provide: DeleteTodoUseCase.UseCase,
-      useFactory: (todoRepo: TodoRepository.Repository) => {
-        return new DeleteTodoUseCase.UseCase(todoRepo);
-      },
-      inject: ['TodoInMemoryRepository'],
-    },
-    {
-      provide: GetTodoUseCase.UseCase,
-      useFactory: (todoRepo: TodoRepository.Repository) => {
-        return new GetTodoUseCase.UseCase(todoRepo);
-      },
-      inject: ['TodoInMemoryRepository'],
-    },
-    {
-      provide: ListTodosUseCase.UseCase,
-      useFactory: (todoRepo: TodoRepository.Repository) => {
-        return new ListTodosUseCase.UseCase(todoRepo);
-      },
-      inject: ['TodoInMemoryRepository'],
-    },
-    {
-      provide: UpdateTodoUseCase.UseCase,
-      useFactory: (todoRepo: TodoRepository.Repository) => {
-        return new UpdateTodoUseCase.UseCase(todoRepo);
-      },
-      inject: ['TodoInMemoryRepository'],
-    },
+    ...Object.values(TODO_PROVIDERS.REPOSITORIES),
+    ...Object.values(TODO_PROVIDERS.USE_CASES),
   ],
 })
 export class TodosModule {}
