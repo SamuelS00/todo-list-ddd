fcc1dad61c5b82b578577e784fa1fa4a4330962f
Samuel <samuelsoutosilva14@gmail.com>
2023-01-26 20:12:29 -0300

    refactor: remove console.log

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Entity } from 'todo-list/@shared/domain';
-import { Todo } from 'todo-list/todo/domain';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
   getHello(): string {
-    console.log(Todo);
-    console.log(Entity);
     return this.appService.getHello();
   }
 }
