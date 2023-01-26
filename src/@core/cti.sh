ecfc5f951ba5d668c30f64adc89f5a1ab16bee20
Samuel <samuelsoutosilva14@gmail.com>
2023-01-26 20:09:10 -0300

    refactor: ignore tests directory

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 npm run cti create './src/@shared/application' -- -i '*spec.ts' -b &&
-npm run cti create './src/@shared/domain' -- -i '*spec.ts' -b &&
+npm run cti create './src/@shared/domain' -- -i '*spec.ts' -e 'tests' -b &&
+npm run cti create './src/@shared/todo/infrastructure' -- -i '*spec.ts' -b &&
 
-npm run cti create './src/modules/todo/application' -- -i '*spec.ts' -b &&
-npm run cti create './src/modules/todo/domain' -- -i '*spec.ts' -b &&
-npm run cti create './src/modules/todo/infrastructure' -- -i '*spec.ts' -b
+npm run cti create './src/todo/application' -- -i '*spec.ts' -b &&
+npm run cti create './src/todo/domain' -- -i '*spec.ts' -b &&
+npm run cti create './src/todo/infrastructure' -- -i '*spec.ts' -b
