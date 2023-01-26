178f530344d7d67633d5ea03e143085654a2ba30
Samuel <samuelsoutosilva14@gmail.com>
2023-01-26 20:16:10 -0300

    feat: add sequelize module in  todo module

--- src/nestjs/src/todos/todos.module.ts
+++ src/nestjs/src/todos/todos.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
 import { TODO_PROVIDERS } from './providers/todo.providers';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { TodoSequelize } from 'todo-list/todo/infrastructure';
 
 @Module({
+  imports: [SequelizeModule.forFeature([TodoSequelize.TodoModel])],
   controllers: [TodosController],
   providers: [
     TodosService,
