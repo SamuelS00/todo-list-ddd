4db6dc7ea7ac79bf31b60bcd8578959256b51fd4
Samuel <samuelsoutosilva14@gmail.com>
2023-01-26 20:18:05 -0300

    feat: add environment configuration and database module in main module imports

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [ConfigModule.forRoot(), TodosModule, DatabaseModule],
 })
 export class AppModule {}
