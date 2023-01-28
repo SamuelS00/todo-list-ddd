d60aee058f0ba62aa75321c2e814d47b71464592
Samuel <samuelsoutosilva14@gmail.com>
2023-01-27 21:56:37 -0300

    refactor: remove priority generate

--- src/@core/src/@shared/infrastructure/testing/helpers/generate-priority-option.ts
+++
@@ -1,7 +0,0 @@
-import { Priority } from '#todo/domain/entities/todo'
-
-const priorityOptions: Priority[] = ['low', 'medium', 'high']
-
-export function genPriorityOption (): Priority {
-  return priorityOptions[Math.floor(Math.random() * priorityOptions.length)]
-}
