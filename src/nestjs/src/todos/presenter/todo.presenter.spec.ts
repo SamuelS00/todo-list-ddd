ac9be03c12a7d55c2a82feb8d13217bbd5a70337
Samuel <samuelsoutosilva14@gmail.com>
2023-01-31 20:10:42 -0300

    test: add unit tests for todo collection presenter

--- src/nestjs/src/todos/presenter/todo.presenter.spec.ts
+++ src/nestjs/src/todos/presenter/todo.presenter.spec.ts
@@ -1,5 +1,6 @@
-import { TodoPresenter } from './todo.presenter';
+import { TodoCollectionPresenter, TodoPresenter } from './todo.presenter';
 import { instanceToPlain } from 'class-transformer';
+import { PaginationPresenter } from '../../@shared/presenters/pagination.presenter';
 
 describe('TodoPresenter Unit Tests', () => {
   describe('constructor', () => {
@@ -42,6 +43,105 @@ describe('TodoPresenter Unit Tests', () => {
       description: 'go to the central market',
       is_scratched: false,
       created_at: created_at.toISOString(),
+    });
+  });
+});
+
+describe('TodoCollectionPresenter Unit Tests', () => {
+  describe('constructor', () => {
+    it('should set values', () => {
+      const todoProps = {
+        id: '22c7bbc8-b798-481e-b9fd-5bacd3c235c6',
+        title: 'Supermarket',
+        priority: 1,
+        description: 'buy fruits',
+        is_scratched: false,
+        created_at: new Date(),
+      };
+
+      const presenter = new TodoCollectionPresenter({
+        items: [todoProps],
+        current_page: 1,
+        per_page: 2,
+        last_page: 3,
+        total: 4,
+      });
+
+      expect(presenter.meta).toBeInstanceOf(PaginationPresenter);
+      expect(presenter.meta).toEqual(
+        new PaginationPresenter({
+          current_page: 1,
+          per_page: 2,
+          last_page: 3,
+          total: 4,
+        }),
+      );
+      expect(presenter.data).toStrictEqual([new TodoPresenter(todoProps)]);
+    });
+  });
+
+  it('should present data', () => {
+    const todoProps = {
+      id: '22c7bbc8-b798-481e-b9fd-5bacd3c235c6',
+      title: 'Supermarket',
+      priority: 1,
+      description: 'buy fruits',
+      is_scratched: false,
+      created_at: new Date(),
+    };
+
+    let presenter = new TodoCollectionPresenter({
+      items: [todoProps],
+      current_page: 1,
+      per_page: 2,
+      last_page: 3,
+      total: 4,
+    });
+
+    expect(instanceToPlain(presenter)).toStrictEqual({
+      meta: {
+        current_page: 1,
+        per_page: 2,
+        last_page: 3,
+        total: 4,
+      },
+      data: [
+        {
+          id: todoProps.id,
+          title: todoProps.title,
+          priority: todoProps.priority,
+          description: todoProps.description,
+          is_scratched: todoProps.is_scratched,
+          created_at: todoProps.created_at.toISOString(),
+        },
+      ],
+    });
+
+    presenter = new TodoCollectionPresenter({
+      items: [todoProps],
+      current_page: '1' as any,
+      per_page: '2' as any,
+      last_page: '3' as any,
+      total: '4' as any,
+    });
+
+    expect(instanceToPlain(presenter)).toStrictEqual({
+      meta: {
+        current_page: 1,
+        per_page: 2,
+        last_page: 3,
+        total: 4,
+      },
+      data: [
+        {
+          id: todoProps.id,
+          title: todoProps.title,
+          priority: todoProps.priority,
+          description: todoProps.description,
+          is_scratched: todoProps.is_scratched,
+          created_at: todoProps.created_at.toISOString(),
+        },
+      ],
     });
   });
 });
