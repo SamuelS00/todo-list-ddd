3c838720aec83105a9b6d5773b34cc756b9c35b0
Samuel <samuelsoutosilva14@gmail.com>
2023-01-31 20:10:08 -0300

    feat: add todo collection presenter

--- src/nestjs/src/todos/presenter/todo.presenter.ts
+++ src/nestjs/src/todos/presenter/todo.presenter.ts
@@ -1,5 +1,6 @@
-import { TodoOutput } from 'todo-list/todo/application';
-import { Exclude, Transform } from 'class-transformer';
+import { ListTodosUseCase, TodoOutput } from 'todo-list/todo/application';
+import { Transform } from 'class-transformer';
+import { CollectionPresenter } from '../../@shared/presenters/collection.presenter';
 
 export class TodoPresenter {
   id: string;
@@ -17,5 +18,15 @@ export class TodoPresenter {
     this.description = output.description;
     this.is_scratched = output.is_scratched;
     this.created_at = output.created_at;
+  }
+}
+
+export class TodoCollectionPresenter extends CollectionPresenter {
+  data: TodoPresenter[];
+
+  constructor(output: ListTodosUseCase.Output) {
+    const { items, ...paginationProps } = output;
+    super(paginationProps);
+    this.data = items.map((item) => new TodoPresenter(item));
   }
 }
