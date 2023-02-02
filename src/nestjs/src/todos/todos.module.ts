7a6f3922fe18f46d636b4b4cb570b7f99aefe067
Samuel <samuelsoutosilva14@gmail.com>
2023-02-02 20:18:14 -0300

    refactor: remove todos service module

--- src/nestjs/src/todos/todos.module.ts
+++ src/nestjs/src/todos/todos.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
-import { TODO_PROVIDERS } from './providers/todo.providers';
+import { TODO_PROVIDERS } from './providers/todo.provider';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { TodoSequelize } from 'todo-list/todo/infrastructure';
 
@@ -9,7 +8,6 @@ import { TodoSequelize } from 'todo-list/todo/infrastructure';
   imports: [SequelizeModule.forFeature([TodoSequelize.TodoModel])],
   controllers: [TodosController],
   providers: [
-    TodosService,
     ...Object.values(TODO_PROVIDERS.REPOSITORIES),
     ...Object.values(TODO_PROVIDERS.USE_CASES),
   ],
