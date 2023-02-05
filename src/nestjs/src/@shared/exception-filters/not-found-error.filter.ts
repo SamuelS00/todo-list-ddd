3e09314c849725e709df9de74b2e1606649b5c8d
Samuel <samuelsoutosilva14@gmail.com>
2023-02-04 21:37:41 -0300

    feat: add not found error filter

---
+++ src/nestjs/src/@shared/exception-filters/not-found-error.filter.ts
@@ -0,0 +1,17 @@
+import { NotFoundError } from 'todo-list/@shared/domain';
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(NotFoundError)
+export class NotFoundErrorFilter implements ExceptionFilter {
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(404).json({
+      statusCode: 404,
+      error: 'Not Found',
+      message: exception.message,
+    });
+  }
+}
