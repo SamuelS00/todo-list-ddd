6f8ba9c3f477fd9842dad1e21fec4eed6e13b466
Samuel <samuelsoutosilva14@gmail.com>
2023-02-04 21:30:02 -0300

    feat: add global-config

---
+++ src/nestjs/src/global-config.ts
@@ -0,0 +1,25 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { EntityValidationErrorFilter } from './@shared/exception-filters/entity-validation-error.filter';
+import { NotFoundErrorFilter } from './@shared/exception-filters/not-found-error.filter';
+import { WrapperDataInterceptor } from './@shared/interceptors/wrapper-data.interceptor';
+
+export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+    }),
+  );
+  app.useGlobalInterceptors(
+    new WrapperDataInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+  app.useGlobalFilters(
+    new EntityValidationErrorFilter(),
+    new NotFoundErrorFilter(),
+  );
+}
