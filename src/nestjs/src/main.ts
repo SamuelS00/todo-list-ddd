0e323e817fbfed1544e48fcb6db81536df654074
Samuel <samuelsoutosilva14@gmail.com>
2023-02-04 21:42:33 -0300

    feat: add applyGlobalConfig

--- src/nestjs/src/main.ts
+++ src/nestjs/src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { applyGlobalConfig } from './global-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  applyGlobalConfig(app);
   await app.listen(3000);
 }
 bootstrap();
