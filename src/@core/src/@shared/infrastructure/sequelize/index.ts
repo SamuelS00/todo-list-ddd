f11b4d66151a7c4ec9ff616fcf774680393801c9
Samuel <samuelsoutosilva14@gmail.com>
2023-02-06 20:34:23 -0300

    feat: re-add indexes

---
+++ src/@core/src/@shared/infrastructure/sequelize/index.ts
@@ -0,0 +1,5 @@
+// created from 'create-ts-index'
+
+export * from './migrator-cli';
+export * from './migrator';
+export * from './sequelize-model.factory';
---
+++ src/@core/src/todo/infrastructure/db/sequelize/migrations/index.ts
@@ -0,0 +1,3 @@
+// created from 'create-ts-index'
+
+export * from './2023.02.06T19.20.11.create-todos-table';
