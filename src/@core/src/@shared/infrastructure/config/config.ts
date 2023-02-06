8625afa43d41b485ef8687466b4f6df00d40b582
Samuel <samuelsoutosilva14@gmail.com>
2023-02-06 20:55:10 -0300

    feat: add config

---
+++ src/@core/src/@shared/infrastructure/config/config.ts
@@ -0,0 +1,24 @@
+import { config as readEnv } from 'dotenv'
+import { join } from 'path'
+
+interface ConfigOutput {
+  db: {
+    vendor: any
+    host: string | undefined
+    logging: boolean
+  }
+}
+
+function makeConfig (envFile: string): ConfigOutput {
+  const output = readEnv({ path: envFile })
+
+  return {
+    db: {
+      vendor: output.parsed?.DB_VENDOR as any,
+      host: output.parsed?.DB_HOST,
+      logging: output.parsed?.DB_LOGGING === 'true'
+    }
+  }
+}
+const envTestingFile = join(__dirname, '../../../../.env.test')
+export const configTest = makeConfig(envTestingFile)
