248443aab9539e151a2e1c3d6fc61b3ba0490da0
Samuel <samuelsoutosilva14@gmail.com>
2023-02-07 22:38:58 -0300

    chore: ignore migrator in cti

--- src/@core/cti.sh
+++ src/@core/cti.sh
@@ -2,7 +2,7 @@
 
 npm run cti create './src/@shared/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/@shared/domain' -- -i '*spec.ts' -e 'tests' -b &&
-npm run cti create './src/@shared/infrastructure' -- -i '*spec.ts' -b &&
+npm run cti create './src/@shared/infrastructure' -- -i '*spec.ts' -i 'migrator-cli.ts' -b &&
 
 npm run cti create './src/todo/application' -- -i '*spec.ts' -b &&
 npm run cti create './src/todo/domain' -- -i '*spec.ts' -b &&
