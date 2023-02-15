bd371d3b9aacd303f68c502dcf415d101887ab60
Samuel <samuelsoutosilva14@gmail.com>
2023-02-15 20:29:02 -0300

    chore: add cors config

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { SharedModule } from './@shared/@shared.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import * as cors from 'cors';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [ConfigModule.forRoot(), TodosModule, DatabaseModule, SharedModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors({ origin: 'http://localhost:3001' })).forRoutes('*');
+  }
+}
